5ffe93fbdc39ef611d9b682b79529e5eb440484e
Yameen <chrisfdev@gmail.com>
2025-11-25 22:05:36 -0800

    Homepage redesign

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,3 +4,6 @@
 
 export const APP_TITLE = "RapGPT";
 export const TAGLINE = "Don't Matter Just Don't Byte It";
+export const TAGLINE_2 = "No Byting";
+export const MADE_BY = "Good At Coding";
+export const YEAR = "2025";
