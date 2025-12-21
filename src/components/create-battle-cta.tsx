19800cad76962b71b5278ab67b89c885442bbec3
Yameen <chrisfdev@gmail.com>
2025-12-21 11:35:18 -0800

    Update buttons on marketing homepage to be Clerk Sign Up button

--- src/components/create-battle-cta.tsx
+++ src/components/create-battle-cta.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SignInButton } from "@clerk/nextjs";
+import { SignUpButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 interface CreateBattleCTAProps {
@@ -24,10 +24,10 @@ export function CreateBattleCTA({
   }
 
   return (
-    <SignInButton mode="modal" forceRedirectUrl="/new-battle">
+    <SignUpButton mode="modal" forceRedirectUrl="/new-battle">
       <button className="inline-block px-8 py-4 bg-linear-to-r from-yellow-500 to-red-600 hover:from-yellow-600 hover:to-red-700 rounded-lg text-white font-bold text-lg transition-all transform hover:scale-105 shadow-lg">
         {title}
       </button>
-    </SignInButton>
+    </SignUpButton>
   );
 }
