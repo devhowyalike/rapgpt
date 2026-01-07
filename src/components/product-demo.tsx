4a121bad2f55d8b30b83f701b78a5779cf2a40e6
Yameen <chrisfdev@gmail.com>
2026-01-07 12:47:01 -0800

    Feature Card mobile updates

--- src/components/product-demo.tsx
+++ src/components/product-demo.tsx
@@ -51,7 +51,7 @@ export function ScreenshotShowcaseStatic({
     {
       icon: <Music className="w-6 h-6" />,
       title: "Make it a Song",
-      description: "Select a style and generate it with AI.",
+      description: "Select a style & generate it with AI.",
       color: "green",
     },
   ];
