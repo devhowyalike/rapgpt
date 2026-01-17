96ae202fbfbc22cf687f9c4e72046ac3bac173a9
Yameen <chrisfdev@gmail.com>
2026-01-16 22:27:13 -0800

    Marketing updates

--- src/app/learn-more/page.tsx
+++ src/app/learn-more/page.tsx
@@ -10,7 +10,7 @@ import { APP_TITLE } from "@/lib/constants";
 
 export const metadata = createMetadata({
   title: "Learn More",
-  description: `Explore the interface and features of ${APP_TITLE}. Choose your MCs, set the stage, and watch the battle unfold.`,
+  description: `Explore the interface and features of ${APP_TITLE}. Choose your MCs, customize the rhimes, and watch it unfold.`,
   path: "/learn-more",
 });
 
